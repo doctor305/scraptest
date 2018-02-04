808c6072e16e3d7c54ba61467b443deddfa11efe
doctor305 <94644276@qq.com>
2018-02-04 22:23:08 +0800

    update

--- code/crawler/scrapyProject/getProxy/getProxy/pipelines.py
+++ code/crawler/scrapyProject/getProxy/getProxy/pipelines.py
@@ -8,4 +8,12 @@
 
 class GetproxyPipeline(object):
     def process_item(self, item, spider):
+        filename = 'proxy.txt'
+        with open(filename,'a') as fp:
+            fp.write(item['ip'].encode('utf8').strip()+'\t')
+            fp.write(item['port'].encode('utf8').strip()+'\t')
+            fp.write(item['protocol'].encode('utf8').strip()+'\t')
+            fp.write(item['typ'].encode('utf8').strip()+'\t')
+            fp.write(item['loction'].encode('utf8').strip()+'\t')
+            fp.write(item['source'].encode('utf8').strip()+'\n')
         return item
--- code/crawler/scrapyProject/getProxy/getProxy/spiders/proxyspider.py
+++ code/crawler/scrapyProject/getProxy/getProxy/spiders/proxyspider.py
@@ -1,11 +1,30 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from getProxy.items import GetproxyItem
 
 class ProxyspiderSpider(scrapy.Spider):
     name = 'proxyspider'
     allowed_domains = ['xicidaili.com']
-    start_urls = ['http://xicidaili.com/']
+    wds = ['nn','nt','wn','wt']
+    pages = 20
+    start_urls = []
+    for types in wds:
+        for i in xrange(1,pages+1):
+            start_urls.append('http://www.xicidaili.com/'+types+'/'+str(i))
+            
 
     def parse(self, response):
-        pass
+        subSelector = response.xpath('//tr[@class=""]|//tr[@class="odd"]')
+        items = []
+        for sub in subSelector:
+            item = GetproxyItem()
+            item['ip'] = sub.xpath('.//td[2]/text()').extract()[0]
+            item['port'] = sub.xpath('.//td[3]/text()').extract()[0]
+            item['typ'] = sub.xpath('.//td[5]/text()').extract()[0]
+            if sub.xpath('.//td[4]/a/text()'):
+                item['loction'] = sub.xpath('.//td[4]/a/text()').extract()[0]
+            else:
+                item['loction'] = sub.xpath('.//td[4]/text()').extract()[0]
+            item['protocol'] = sub.xpath('.//td[6]/text()').extract()[0]
+            item['source'] = 'xicidaili'
+        return items
